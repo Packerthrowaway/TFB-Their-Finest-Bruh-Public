b20b01b91409a98c4de57b8ba1625d79633243af
SwimmySwag <SwimmySwag@email.invalid>
2021-12-16 19:01:45 +0100

    fix for ctd

--- common/defines/TFB_defines.lua
+++ common/defines/TFB_defines.lua
@@ -370,5 +370,5 @@ NDefines.NIntel.ARMY_ARMY_COUNT_RANGE_INTEL_MIN = 0.1
 NDefines.NIntel.NAVAL_SUPREMACY_INTEL_LOW = 0.3								-- we need more intel than this to get any supremacy
 NDefines.NIntel.NAVY_DEPLOYED_MANPOWER_COUNT_RANGE_INTEL_MIN = 0.1
 
-NDefines.NSupply.CAPITAL_SUPPLY_CIVILIAN_FACTORIES = 0.2, -- supply from one civilian factory
-NDefines.NSupply.CAPITAL_SUPPLY_MILITARY_FACTORIES = 0.3, -- supply from one military factory
+NDefines.NSupply.CAPITAL_SUPPLY_CIVILIAN_FACTORIES = 0.2 -- supply from one civilian factory
+NDefines.NSupply.CAPITAL_SUPPLY_MILITARY_FACTORIES = 0.3 -- supply from one military factory
