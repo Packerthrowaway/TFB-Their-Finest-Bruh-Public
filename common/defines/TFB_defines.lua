bc6ba85c33a2e5bdcfe936431d2f3d8f8050cd30
Packerfan504 <Packerthrowaway@gmail.com>
2022-07-05 14:33:31 -0500

    Copied in the Defines for Scout Planes from Vanilla to edit later

--- common/defines/TFB_defines.lua
+++ common/defines/TFB_defines.lua
@@ -393,6 +393,10 @@ NDefines.NIntel.ARMY_ARMY_COUNT_RANGE_INTEL_MIN = 0.1
 NDefines.NIntel.NAVAL_SUPREMACY_INTEL_LOW = 0.3								-- we need more intel than this to get any supremacy
 NDefines.NIntel.NAVY_DEPLOYED_MANPOWER_COUNT_RANGE_INTEL_MIN = 0.1
 
+NDefines.NIntel.RECON_PLANE_INTEL_BASE = 0.02 				-- intel base amount for a strategic area per plane
+NDefines.NIntel.RECON_PLANE_LAND_DISTRIBUTION = { 10.0, 6.0, 0.0, 3.0 }    -- controls for land and sea zones how much of each intel typee is given (civ, army, navy, air)
+NDefines.NIntel.RECON_PLANE_SEA_DISTRIBUTION = { 0.0, 0.0, 10.0, 0.0 }	-- Note that this is the MAXIMUM amount of intel given to the total intel type, IE Scout Planes can give no more than 10% Naval Intel
+
 NDefines.NSupply.CAPITAL_SUPPLY_CIVILIAN_FACTORIES = 0.2 -- supply from one civilian factory
 NDefines.NSupply.CAPITAL_SUPPLY_MILITARY_FACTORIES = 0.3 -- supply from one military factory
 -- The range bonus added to a fully motorized hub. This supply is added on top of the XXX_INITIAL_SUPPLY_FLOW defined above.
