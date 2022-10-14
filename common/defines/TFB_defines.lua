6cf917789c7297b314533f7979970ab58080de9f
Packerfan504 <Packerthrowaway@gmail.com>
2022-10-14 11:08:18 -0500

    I need to fix this fucking defines file already

--- common/defines/TFB_defines.lua
+++ common/defines/TFB_defines.lua
@@ -239,10 +239,10 @@ NDefines.NMilitary.DAMAGE_SPLIT_ON_FIRST_TARGET = 0.2				--% of damage dealt to 
 NDefines.NAir.AIR_WING_MAX_STATS_ATTACK = 120
 NDefines.NAir.AIR_WING_MAX_STATS_DEFENCE = 120
 NDefines.NAir.AIR_WING_MAX_STATS_AGILITY = 120
-NDefines.Nair.TOP_SPEED_DAMAGE_BONUS_FACTOR = 0.2,				-- A factor for scaling the top speed of a plane into damage buff --Vanilla is .02
-NDefines.Nair.COMBAT_DAMAGE_STATS_MULTILPIER = 0.3,	
-NDefines.Nair.BIGGEST_AGILITY_FACTOR_DIFF = 4.0,					-- biggest factor difference in agility for doing damage (caps to this) -- Vanilla is 3.0
-NDefines.Nair.BIGGEST_SPEED_FACTOR_DIFF = 3.0,					-- biggest factor difference in speed for doing damage (caps to this)	-- Vanilla is 2.5	
+NDefines.NAir.TOP_SPEED_DAMAGE_BONUS_FACTOR = 0.2				-- A factor for scaling the top speed of a plane into damage buff --Vanilla is .02
+NDefines.NAir.COMBAT_DAMAGE_STATS_MULTILPIER = 0.3	
+NDefines.NAir.BIGGEST_AGILITY_FACTOR_DIFF = 4.0					-- biggest factor difference in agility for doing damage (caps to this) -- Vanilla is 3.0
+NDefines.NAir.BIGGEST_SPEED_FACTOR_DIFF = 3.0					-- biggest factor difference in speed for doing damage (caps to this)	-- Vanilla is 2.5	
 NDefines.NAir.AIR_WING_BOMB_DAMAGE_FACTOR = 2					-- Used to balance the damage done while bombing.
 NDefines.NAir.COMBAT_MULTIPLANE_CAP = 2.5						-- How many planes can shoot at each plane on other side ( if there are 100 planes we are atttacking COMBAT_MULTIPLANE_CAP * 100 of our planes can shoot )
 NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.05							-- Higher value = more shot down planes
